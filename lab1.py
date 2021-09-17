460f7946ecd88d5ad060c5f3c15157e9a764924f
DecKirin <lxd199860@gmail.com>
2021-09-17 14:55:33 -0600

    Done

--- lab1.py
+++ lab1.py
@@ -7,3 +7,11 @@ print("The current version is: ", requests.__version__)
 hp = requests.get("http://www.google.com")
 print(hp)
 
+#This script on git hub
+git_url = "https://raw.githubusercontent.com/DecKirin/CMPUT404LAB1/main/lab1.py"
+source_code = requests.get(git_url)
+print(source_code.text)
+#Download the source code
+download_file = open("downloaded_lab1_xiding.py","wb")
+download_file.write(source_code.content)
+download_file.close()
